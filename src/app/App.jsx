5fb02ee501426916efe95ecd3639f1d5a6eb33fa
Chirag Kapil <your.email@example.com>
2024-09-27 19:18:24 +0530

    second commit today

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -230,12 +230,12 @@ function App() {
 			<div id='About' className={style.about}>
 				<div className={style.container}>
 					<h2 className={style.title}>About Me</h2>
-					<p>Here you will find more information about me, what I do, and my current skills mostly in terms of programming and technology</p>
+					<p>Here you will find more information about me, what I do, and my current skills mostly in terms of programming and technology.</p>
 					<div className={style["about-content"]}>
 						<div className={style["about-info"]}>
 							<h3>Get to know me!</h3>
 							<p>
-								I'm a <span>Frontend Web Developer</span> building the Front-end of Websites and Web Applications that leads to the success of the overall product. Check out some of my work in the <span>Projects</span> section. <br /> <br />
+								I'm a <span>Frontend Web Developer</span> building the Front-end of Websites & Web Applications that leads to the success of the overall product. Check out some of my work in the <span>Projects</span> section. <br /> <br />
 								I also like sharing content related to the stuff that I have learned over the years in <span>Web Development</span> so it can help other people of the Dev Community. Feel free to Connect or Follow me on my <a href="https://github.com/ChiragKapil03" target="_blank">Github</a> where I post useful content related to Web Development and Programming. <br /> <br />
 								I'm open to <span>Job</span> opportunities where I can contribute, learn and grow. If you have a good opportunity that matches my skills and experience then don't hesitate to <span>contact</span> me.
 							</p>
